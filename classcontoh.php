25c42d326e4c7fdae6fedacb934592562c3a04d0
diniwdwi <84960157+diniwdwi@users.noreply.github.com>
2021-05-28 09:17:19 +0100

    Add files via upload

---
+++ classcontoh.php
@@ -0,0 +1,32 @@
+<?php
+class manusia{
+    var $nama;
+    var $warna_kulit;
+
+    function tampilkan_nama(){
+        return $this->nama;
+    }
+    function warna_kulit(){
+        return $this->warna_kulit;
+    }
+    function set_nama($nama){
+        $this->nama=$nama;
+    }
+    function set_warna_kulit($tmp){
+        $this->warna_kulit=$tmp;
+    }
+}
+$manusia = new manusia();
+$manusia->set_nama("Dini Dwi Rahayu");
+$manusia->set_warna_kulit("Putih kali <br>");
+echo $manusia->tampilkan_nama();
+echo "<br>";
+echo $manusia->warna_kulit();
+echo "<br>";
+$manusia2 = new manusia();
+$manusia2->set_nama("Dinzy Rahayu");
+$manusia2->set_warna_kulit("Sawo Matang");
+echo $manusia2->tampilkan_nama();
+echo "<br>";
+echo $manusia2->warna_kulit();
+?>
